8a3a486068cf44fd1335801a11443a209e236b52
kreativekommit <mohamed@kreativekommit.com>
2020-11-05 11:18:31 +0200

    added sliders to the site

--- src/components/home/we think/components/slider/index.js
+++ src/components/home/we think/components/slider/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import Carousel from 'react-elastic-carousel';
+import Item from './item.js';
 import Styles from './slider.module.scss';
 
 const Slider = props => {
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2 },
+        { width: 768, itemsToShow: 3 },
+        { width: 1200, itemsToShow: 4 },
+      ];
     return (
-        <div>
-            Slider
+        <>
+        <div className = {`${Styles.slider}`}>
+          <Carousel>
+            <Item>One</Item>
+            <Item>Two</Item>
+            <Item>Three</Item>
+            <Item>Four</Item>
+            <Item>Five</Item>
+            <Item>Six</Item>
+            <Item>Seven</Item>
+            <Item>Eight</Item>
+          </Carousel>
         </div>
+      </>
     )
 }
 export default Slider;
